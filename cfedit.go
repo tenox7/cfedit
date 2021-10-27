058ea5c35ad502afd2325a95503757176d3cdee0
Antoni Sawicki <tenox@google.com>
2021-10-26 20:45:37 -0700

    no auth if there are no users defined

--- cfedit.go
+++ cfedit.go
@@ -1,12 +1,12 @@
 /*
  * Copyright 2021 Google LLC
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     https://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -226,6 +226,9 @@ func (c *bmClient) WriteFile(ctx context.Context, f string) {
 }
 
 func (c *bmClient) Auth(ctx context.Context) bool {
+	if len(users) == 0 {
+		return true
+	}
 	var s string
 	u, p, ok := c.r.BasicAuth()
 	if !ok {
